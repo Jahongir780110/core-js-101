2dd7f3e7755e265992c84fcb8a85fbe70bbe6851
Jahongir780110 <jahongir780110@gmail.com>
2022-07-15 16:40:02 +0500

    fix: reverted eslint config and removed example.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
-    'comma-dangle': 0,
+    // 'comma-dangle': 0,
   },
 };
--- src/example.js
+++
@@ -1,5 +0,0 @@
-function sortDigitNamesByNumericOrder(arr) {
-  return arr.sort();
-}
-
-console.log(sortDigitNamesByNumericOrder(['nine', 'one']));
